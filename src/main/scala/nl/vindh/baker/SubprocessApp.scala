fb1deeefb344cdb1c66772d544d817f909eff26b
Vincent de Haan <vincentdehaan@gmail.com>
2019-04-21 00:52:38 +0200

    Now the unexpanded process compiles as well

--- src/main/scala/nl/vindh/baker/SubprocessApp.scala
+++ src/main/scala/nl/vindh/baker/SubprocessApp.scala
@@ -5,15 +5,19 @@ import com.ing.baker.recipe.scaladsl.{Event, Ingredient, Interaction, Recipe}
 
 object SubprocessApp extends App with WebshopConcepts with Subprocesses {
   // We define several subprocesses
+  val generalGoodAcquisitionProcess = ExpandableSubprocess(
+    name = "GeneralGoodAcquisitionProcess",
+    inputIngredients = Seq(order),
+    outputIngredients = Seq(goods)
+  )
+
   val manufactureSubprocess: Recipe =
     Recipe("ManufactureSubprocess")
-      .withInteraction(manufactureGoods
-        .withRequiredEvents(valid, paymentMade))
+      .withInteraction(manufactureGoods)
 
   val retrieveSubprocess: Recipe =
     Recipe("RetrieveSubprocess")
-    .withInteraction(retrieveGoods
-      .withRequiredEvents(valid, paymentMade))
+    .withInteraction(retrieveGoods)
 
   val webShopRecipe: Recipe =
     Recipe("WebShop")
@@ -21,19 +25,23 @@ object SubprocessApp extends App with WebshopConcepts with Subprocesses {
         validateOrder,
         shipGoods,
         sendInvoice
-          .withRequiredEvent(goodsShipped)
+          .withRequiredEvent(goodsShipped),
+        generalGoodAcquisitionProcess
+          .withRequiredEvents(valid, paymentMade)
       )
       .withSensoryEvents(
         customerInfoReceived,
         orderPlaced,
         paymentMade)
 
-  val webshopWithFactory = webShopRecipe.withSubprocess(manufactureSubprocess)
-  val webshopWithWarehouse = webShopRecipe.withSubprocess(retrieveSubprocess)
+  val webshopWithFactory = webShopRecipe.expandSubprocess(generalGoodAcquisitionProcess, manufactureSubprocess)
+  val webshopWithWarehouse = webShopRecipe.expandSubprocess(generalGoodAcquisitionProcess, retrieveSubprocess)
 
+  val compiledWebshop = RecipeCompiler.compileRecipe(webShopRecipe)
   val compiledWebshopWithFactory = RecipeCompiler.compileRecipe(webshopWithFactory)
   val compiledWebshopWithWarehouse = RecipeCompiler.compileRecipe(webshopWithWarehouse)
 
+  println(compiledWebshop.getRecipeVisualization)
   println(compiledWebshopWithFactory.getRecipeVisualization)
   println(compiledWebshopWithWarehouse.getRecipeVisualization)
  }
--- src/main/scala/nl/vindh/baker/Subprocesses.scala
+++ src/main/scala/nl/vindh/baker/Subprocesses.scala
@@ -1,12 +1,35 @@
 package nl.vindh.baker
 
-import com.ing.baker.recipe.scaladsl.Recipe
+import com.ing.baker.recipe.common.Ingredient
+import com.ing.baker.recipe.scaladsl.{Event, Interaction, Recipe}
 
 trait Subprocesses {
+  object ExpandableSubprocess {
+    def apply(name: String, inputIngredients: Seq[Ingredient], outputIngredients: Seq[Ingredient]): Interaction with ExpandableSubprocess =
+      Interaction(name = name, inputIngredients = inputIngredients, output = Seq(DummyEvent(name, outputIngredients)))
+        .asInstanceOf[Interaction with ExpandableSubprocess]
+  }
+
+  class ExpandableSubprocess
+
+  object DummyEvent {
+    def apply(name: String, outputIngredients: Seq[Ingredient]): Event =
+      Event(s"DummyEvent#$name", outputIngredients:_*)
+  }
+
   implicit class RecipeWithSubprocesses(r: Recipe) {
-    def withSubprocess(sub: Recipe): Recipe =
+    // TODO: decide how to merge the general properties of the subprocess and the main process
+    def expandSubprocess(expandable: Interaction with ExpandableSubprocess, sub: Recipe): Recipe = {
+      val expandableFinal = r.interactions.find(_.name == expandable.name).getOrElse(expandable)
       r.copy(
-        interactions = r.interactions ++ sub.interactions,
-        sensoryEvents = r.sensoryEvents ++ sub.sensoryEvents)
+        interactions = r.interactions.filterNot(_.name == expandable.name) ++ sub.interactions.map {
+          interaction =>
+            if(interaction.requiredEvents.size == 0)
+              interaction.copy(requiredEvents = expandableFinal.requiredEvents)
+            else interaction
+        },
+        sensoryEvents = r.sensoryEvents ++ sub.sensoryEvents
+      )
+    }
   }
 }
